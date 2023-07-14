8d3abe0c05ca7c0aa908975d81af02227108b73d
Nazri Abdullah <nazri.abdullah@me.com>
2023-07-14 23:15:25 +0200

    working for password based file enc

--- decrypt-file.js
+++ decrypt-file.js
@@ -1,88 +1,6 @@
-const axios = require('axios');
-const CryptoJS = require('crypto-js');
-const {writeFileSync, readFileSync, statSync} = require("fs");
-const {readMessage, message, decrypt, decryptKey, readPrivateKey} = require("openpgp");
-
-function decodeBase64Url(base64Url) {
-    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    const padding = base64.length % 4;
-    const paddedBase64 = padding === 0 ? base64 : base64 + '==='.slice(padding);
-    return atob(paddedBase64);
-}
-
-function decryptPrivateKey(encryptedPrivateKey, password) {
-    const decryptedBytes = CryptoJS.AES.decrypt(encryptedPrivateKey, password);
-    return decryptedBytes.toString(CryptoJS.enc.Utf8);
-}
-
-async function decryptFile(encryptedFile, privateKeyArmored, password) {
-    const privateKey = await decryptKey({
-        privateKey: await readPrivateKey({armoredKey: privateKeyArmored}),
-        passphrase: password
-    });
-
-    const encryptedMessage = {
-        message: await message.fromBinary(encryptedFile),
-        privateKeys: [privateKey],
-    };
-
-    const decryptedData = await decrypt({
-        message: encryptedMessage,
-        privateKeys: [privateKey],
-    });
-
-    return decryptedData.data;
-}
-
-function Uint8ArrayToString(uint8Array) {
-    let binaryString = '';
-    uint8Array.forEach((byte) => {
-        binaryString += String.fromCharCode(byte);
-    });
-    return binaryString;
-}
-
-async function getFileFromAPI(url) {
-    const response = await axios.get(url, {responseType: 'arraybuffer'});
-    const headers = response.headers;
-    const fileData = new Uint8Array(response.data);
-    console.log('File length:', fileData.length);
-    return {
-        headers: headers,
-        data: fileData,
-    };
-}
-
-async function processLink(link) {
-    try {
-        // Extract UID and Password from the base64 value in the link
-        const base64 = link.substring(link.lastIndexOf('/') + 1);
-        const decoded = decodeBase64Url(base64);
-        const [uid, password] = decoded.split('.');
-
-        // Fetch the encrypted private key from the API
-        const privateKeyUrl = `http://localhost:7777/secret/${uid}`;
-        const privateKeyResponse = await axios.get(privateKeyUrl);
-        const encryptedPrivateKey = privateKeyResponse.data.secret.message;
-        // Decrypt the private key
-        const privateKey = decryptPrivateKey(encryptedPrivateKey, password);
-        // Download the encrypted file from the API
-        const fileUrl = `http://localhost:7777/file/${uid}`;
-        const {headers, data} = await getFileFromAPI(fileUrl);
-        const ext = headers['x-ext']
-        // Decrypt the file
-        const decryptedFile = await decryptFile(data, privateKey, password);
-
-        // Save the decrypted file locally
-        const filename = `decrypted-${uid}${ext}`;
-        writeFileSync(filename, decryptedFile, 'binary');
-        console.log(`Decrypted file saved as ${filename}`);
-        const fileSize = getFileSize(filename);
-        console.log('File Size (KB):', fileSize.kilobytes);
-    } catch (error) {
-        console.error('Error:', error.message);
-    }
-}
+const { decrypt, readMessage } = require('openpgp');
+const { readFileSync, writeFileSync, statSync } = require('fs');
+const axios = require("axios");
 
 const readLinkFromOutputFile = (filePath) => {
     const content = readFileSync(filePath, 'utf8');
@@ -99,6 +17,55 @@ const getFileSize = (filePath) => {
         kilobytes: fileSizeInKB
     };
 };
+
+function decodeBase64Url(base64Url) {
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const padding = base64.length % 4;
+    const paddedBase64 = padding === 0 ? base64 : base64 + '==='.slice(padding);
+    return atob(paddedBase64);
+}
+
+async function getFileFromAPI(url) {
+    const response = await axios.get(url, { responseType: 'arraybuffer' });
+    const headers = response.headers;
+    return {
+        headers: headers,
+        data: response.data,
+    };
+}
+
+async function processLink(link) {
+    try {
+        // Extract UID and Password from the base64 value in the link
+        const base64 = link.substring(link.lastIndexOf('/') + 1);
+        const decoded = decodeBase64Url(base64);
+        const [uid, password] = decoded.split('.');
+
+        // Download the encrypted file from the API
+        const fileUrl = `http://localhost:7777/file/${uid}`;
+        const { headers, data } = await getFileFromAPI(fileUrl);
+        const ext = headers['x-ext'];
+
+        // Create a message from the encrypted data
+        const encryptedMessage = await readMessage({ binaryMessage: data });
+
+        // Decrypt the message with the password
+        const { data: decrypted } = await decrypt({
+            message: encryptedMessage,
+            passwords: [password],
+            format: 'binary'
+        });
+
+        // Save the decrypted data to a file
+        const filename = `${uid}${ext}`;
+        writeFileSync(filename, decrypted, 'binary');
+        console.log('PDF file encrypted and decrypted successfully.');
+        const fileSize = getFileSize(filename);
+        console.log('File Size (KB):', fileSize.kilobytes);
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+}
 
 const link = readLinkFromOutputFile('output.txt');
 processLink(link);
--- file-encryption.js
+++ file-encryption.js
@@ -1,10 +1,9 @@
-const fs = require('fs');
-const openpgp = require('openpgp');
-const http = require('http');
-const CryptoJS = require('crypto-js');
+const {createMessage, encrypt, readMessage, decrypt} = require('openpgp');
+const { readFileSync, statSync} = require('fs');
+const {post} = require("axios");
 const {extname} = require("path");
-const {statSync} = require("fs");
 
+const filePath = process.argv[2];
 const generateRandomPassword = (length) => {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let password = '';
@@ -13,107 +12,6 @@ const generateRandomPassword = (length) => {
     }
     return password;
 };
-
-const encryptAndSendFile = async (filePath, apiUrl, publicKeyArmored, uid, password) => {
-    const publicKey = await openpgp.readKey({ armoredKey: publicKeyArmored });
-    const readStream = fs.createReadStream(filePath);
-
-    const options = {
-        hostname: '155.4.96.26',
-        port: 7777,
-        path: apiUrl,
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/octet-stream',
-            'X-UID': uid,
-            'X-EXT': extname(filePath),
-        },
-    };
-
-    const req = http.request(options, (res) => {
-        res.on('data', (data) => {
-            // Handle the response from the API
-            console.log('API response:', data.toString());
-        });
-    });
-
-    let totalSize = 0;
-
-    readStream.on('data', async (chunk) => {
-        const encryptedChunk = await openpgp.encrypt({
-            message: await openpgp.createMessage({ binary: chunk }),
-            encryptionKeys: publicKey,
-        });
-        const chunkSize = Buffer.byteLength(encryptedChunk, 'utf8');
-        totalSize += chunkSize;
-        req.write(encryptedChunk);
-    });
-
-    readStream.on('end', () => {
-        req.end();
-        console.log('Total size:', totalSize, 'bytes');
-    });
-};
-
-(async () => {
-    const password = generateRandomPassword(32);
-
-    const { privateKey, publicKey, revocationCertificate } = await openpgp.generateKey({
-        type: 'ecc',
-        curve: 'curve25519',
-        userIDs: [{ name: 'Jon Smith', email: 'jon@example.com' }],
-        passphrase: password,
-        format: 'armored',
-    });
-
-    const privateKeyArmored = privateKey;
-    const publicKeyArmored = publicKey;
-    console.log('Private key:\n', privateKeyArmored);
-    // Encrypt the private key with a random password
-    const encryptedPrivateKey = CryptoJS.AES.encrypt(privateKeyArmored, password).toString();
-
-    // Send the encrypted private key to the API
-    const keyPayload = {
-        once: true,
-        message: encryptedPrivateKey,
-        expiration: 3600,
-    };
-
-    // Send the key payload to the API
-    const keyApiUrl = 'http://155.4.96.26:7777/secret';
-    const keyOptions = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(keyPayload),
-    };
-
-    fetch(keyApiUrl, keyOptions)
-        .then((response) => response.json())
-        .then(async (data) => {
-            if (data.status === 'success') {
-                const uid = data.data.uid;
-                console.log('UID:', uid);
-                const link = Buffer.from(uid + '.' + password).toString('base64');
-                console.log('Link:', 'http://localhost/f/d/' + link);
-                // Store the UID in a constant for further use
-
-                const filePath = 'secret-file.txt'; // Replace with the actual file path
-                const fileSize = getFileSize(filePath);
-                console.log('File size:', fileSize.kilobytes, 'KB');
-                const apiUrl = 'http://155.4.96.26:7777/file'; // Replace with the API URL
-
-                encryptAndSendFile(filePath, apiUrl, publicKeyArmored, uid, password);
-            } else {
-                console.log('Status is not success');
-                // Handle the case when the status is not success
-            }
-        })
-        .catch((error) => {
-            console.error('API Error:', error);
-        });
-})();
 
 const getFileSize = (filePath) => {
     const stats = statSync(filePath);
@@ -124,3 +22,51 @@ const getFileSize = (filePath) => {
         kilobytes: fileSizeInKB
     };
 };
+
+(async () => {
+    try {
+        const password = generateRandomPassword(32); // Set the password for encryption
+
+        // Read the PDF file as Uint8Array
+        const sourceFilePath = filePath;
+        const pdfFile = readFileSync(sourceFilePath);
+        const sourceFileSize = getFileSize(sourceFilePath);
+        console.log('Source File size:', sourceFileSize.kilobytes, 'KB');
+        const pdfData = new Uint8Array(pdfFile);
+
+        // Create a message from the PDF data
+        const message = await createMessage({ binary: pdfData });
+
+        // Encrypt the message with the password
+        const encrypted = await encrypt({
+            message,
+            passwords: [password],
+            format: 'binary'
+        });
+        // console.log('Encrypted:', encrypted); // Uint8Array
+
+// Send the encrypted data to the API
+        const apiUrl = 'http://155.4.96.26:7777/file'; // Replace with the actual API URL
+        const options = {
+            hostname: '155.4.96.26',
+            port: 7777,
+            path: apiUrl,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/octet-stream',
+                'X-EXT': extname(sourceFilePath),
+            },
+        };
+        await post(apiUrl, encrypted, options).then((response) => {
+            const uid = response.data.uid;
+            console.log(`uid: ${response.data.uid}`);
+            const link = Buffer.from(uid + '.' + password).toString('base64');
+            console.log(`Link: http://localhost/f/d/${link}`)
+        }).catch((error) => {
+            console.error(error);
+        });
+        console.log('Encrypted data sent to the API.');
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+})();
